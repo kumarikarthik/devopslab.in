e44b0c6e6e6dfeac74dd71869fc0827ef22758c5
sai krishna <sai482353@gmail.com>
2022-01-01 21:43:50 +0530

    Base commit

---
+++ myscript.ps1
@@ -0,0 +1,11 @@
+for($i=0;$i -le 100;$i++)
+{
+
+Write-Output "I want to say welcome for $i times"
+ if ($i%2 -ne 0){
+     echo "$i is ODD NUMBER..!!"
+ }
+ else{
+     echo "$i is EVEN..!!"
+ }
+}
